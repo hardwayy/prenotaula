0c1c2e8690bc613acd70b64f895502aa441af691
Dav_Tri <152190076+hardwayy@users.noreply.github.com>
2025-05-22 18:34:46 +0200

    reservations pulled from database

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,7 @@
 // In main.rs o in un modulo dedicato (es. models.rs o DTOs.rs)
 
-use chrono::{DateTime, NaiveDateTime, Utc}; // Per gestire e formattare le date
-use rocket::serde::{Serialize, Deserialize}; // Serialize per la risposta
+use chrono::{ NaiveDateTime}; // Per gestire e formattare le date
+use rocket::serde::{Serialize}; // Serialize per la risposta
 
 // Struct per i dati letti dalla query al database
 #[derive(sqlx::FromRow, Debug)] // FromRow per sqlx, Debug per la stampa
